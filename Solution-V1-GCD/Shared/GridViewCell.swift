a782c23f527a846e77e6a31746722d170c505646
Ashish Badak <48381687+ashish-badak@users.noreply.github.com>
2025-12-09 20:51:59 +0530

    Enhancement: configuring shadow inside awakeFromNib to increase scroll performance

--- Solution-V1-GCD/Shared/GridViewCell.swift
+++ Solution-V1-GCD/Shared/GridViewCell.swift
@@ -18,15 +18,27 @@ class GridViewCell: UICollectionViewCell {
         }
     }
 
-    func configure(image: UIImage!) {
-        imageView.image = image
-        imageView.layer.cornerRadius = 25.0
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        
+        /// - NOTE: There is no need to redraw shadow in `configure(image:)` call
+        ///         It gets redrawn everytime cell configuration is invoked
+        ///         So moved it here.
+        ///         Also, adjusted some shadow params to drop visually appealing shadow.
+        imageView.layer.cornerRadius = 15.0
         imageView.clipsToBounds = true
 
-        contentView.layer.shadowColor = UIColor.black.cgColor
-        contentView.layer.shadowRadius = 5.0
-        contentView.layer.shadowOffset = .init(width: 0.0, height: 0.0)
-        contentView.layer.shadowOpacity = 0.5
+        contentView.layer.shadowColor = UIColor.gray.cgColor
+        contentView.layer.shadowRadius = 2
+        contentView.layer.shadowOffset = .zero
+        contentView.layer.shadowOpacity = 0.8
+        contentView.layer.shouldRasterize = true
+        contentView.layer.rasterizationScale = UIScreen.main.scale
+    }
+    
+    func configure(image: UIImage!) {
+        imageView.image = image
     }
 
     override func prepareForReuse() {
