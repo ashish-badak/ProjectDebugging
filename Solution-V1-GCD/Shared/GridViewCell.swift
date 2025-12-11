87ab562057bc66a23f3d7e2d18654163e6ba90e2
Ashish Badak <48381687+ashish-badak@users.noreply.github.com>
2025-12-11 16:27:25 +0530

    Enhancement: added shadow path to shadow

--- Solution-V1-GCD/Shared/GridViewCell.swift
+++ Solution-V1-GCD/Shared/GridViewCell.swift
@@ -3,6 +3,7 @@ import PhotosUI
 
 class GridViewCell: UICollectionViewCell {
     static let livePhotoBadge = PHLivePhotoView.livePhotoBadgeImage(options: .overContent)
+    private static let cornerRadius: CGFloat = 15
 
     @IBOutlet weak var imageView: UIImageView!
     @IBOutlet weak var livePhotoBadgeImageView: UIImageView!
@@ -27,13 +28,26 @@ class GridViewCell: UICollectionViewCell {
         ///         It gets redrawn everytime cell configuration is invoked
         ///         So moved it here.
         ///         Also, adjusted some shadow params to drop visually appealing shadow.
-        imageView.layer.cornerRadius = 15.0
+        imageView.layer.cornerRadius = Self.cornerRadius
         imageView.clipsToBounds = true
 
         contentView.layer.shadowColor = UIColor.gray.cgColor
         contentView.layer.shadowRadius = 2
         contentView.layer.shadowOffset = .zero
         contentView.layer.shadowOpacity = 0.8
+        
+        /// - NOTE: Core Animation computes shadow path internally if not provided.
+        ///         By providing shadow path we help to avoid that computation and make it faster.
+        ///         In example project, cell size is fixed so confiruing `shadowPath` here inside `awakeFromNib()`
+        ///         Else we should do it inside `layoutSubviews()`
+        /// Reference: - https://developer.apple.com/library/archive/documentation/Cocoa/Conceptual/CoreAnimation_guide/ImprovingAnimationPerformance/ImprovingAnimationPerformance.html
+        ///
+        layer.shadowPath = UIBezierPath(
+            roundedRect: bounds,
+            cornerRadius: Self.cornerRadius
+        ).cgPath
+        
+        /// - NOTE: Rasterization helps to cache bitmap and avoids recomputation
         contentView.layer.shouldRasterize = true
         contentView.layer.rasterizationScale = UIScreen.main.scale
     }
